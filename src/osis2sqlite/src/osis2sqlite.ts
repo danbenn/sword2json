56c71e2b78f789041a3e718ac2865f5486945ba8
Daniel Bennett <danbenn@umich.edu>
2018-11-02 00:08:48 -0400

    refactored parser event handlers

--- src/osis2sqlite/src/osis2sqlite.ts
+++ src/osis2sqlite/src/osis2sqlite.ts
@@ -2,8 +2,6 @@ import * as types from './types';
 
 const sax = require('sax');
 
-const parser = sax.parser(true); // strict = true
-
 interface ParserContext {
   lastTag: string;
   currentNode: any;
@@ -51,120 +49,15 @@ function osis2sqlite(verseXML: string, debugOutputEnabled = false) {
     titleText: '',
   };
 
+  const STRICT_MODE = true;
+  const parser = sax.parser(STRICT_MODE);
+
   const verse = [];
 
-  // Handle Parsing errors
+  parser.ontext = (text: string) => parseTextNode(text, verse, context);
+  parser.onopentag = node => parseOpeningTag(node, verse, context);
+  parser.onclosetag = (tagName: string) => parseClosingTag(tagName, verse, context);
   parser.onerror = () => parser.resume();
-
-  // Text node
-  parser.ontext = function (t) {
-    if (context.currentNote) {
-      processFootnotes(t, context);
-    } else if (context.quote) {
-      const strongsNumbers = getStrongsNumbers(context);
-      if (context.quote.attributes.who === 'Jesus' && t) {
-        verse.push([`$redLetter=${t}`]);
-        return;
-      }
-      verse.push([t]);
-      if (strongsNumbers) {
-        verse[verse.length - 1].push(strongsNumbers);
-      }
-    } else if (context.currentNode) {
-      switch (context.currentNode.name) {
-        case OsisTag.SECTION_HEADING:
-          context.titleText += t;
-          break;
-        case OsisTag.DIVINE_NAME:
-          if (context.title) {
-            const strongsNumbers = getStrongsNumbers(context);
-            verse.push([t, strongsNumbers]);
-          }
-          break;
-        default:
-          if ('attributes' in context.currentNode && 'lemma' in context.currentNode.attributes) {
-            const strongsNumbers = getStrongsNumbers(context);
-            verse.push([t, strongsNumbers]);
-            break;
-          }
-          verse.push([t]);
-          break;
-      }
-    } else {
-      verse.push([t]);
-    }
-  };
-
-  // Handle opening tags
-  parser.onopentag = function (node) {
-    // console.log(node);
-    context.currentNode = node;
-    context.lastTag = node.name;
-    switch (node.name) {
-      case OsisTag.XML: // enclosing tag of entire body of content
-        context.osisRef = node.attributes.osisRef;
-        context.verseNum = node.attributes.verseNum;
-        break;
-      case OsisTag.NOTE: // footnote or cross-reference object
-        if (node.attributes.type !== 'crossReference') {
-          const osisRef = node.attributes.osisRef || node.attributes.annotateRef || context.osisRef;
-          if (!node.attributes.n) context.noteCount += 1;
-          const n = node.attributes.n || context.noteCount;
-          verse.push([`$note=${n}&osisRef=${osisRef}`]);
-        }
-        context.currentNote = node;
-        break;
-      case OsisTag.CROSS_REFERENCE: // cross-reference element
-        context.currentRef = node;
-        break;
-      case OsisTag.SECTION_HEADING: // section heading
-        context.title = node;
-        break;
-      case OsisTag.POETIC_LINE: // line indentation
-        if (node.attributes.level === Indentation.small && node.attributes.sID) {
-          verse.push(['$line-break']);
-          verse.push(['$small-indent']);
-        } else if (node.attributes.level === Indentation.large && node.attributes.sID) {
-          verse.push(['$line-break']);
-          verse.push(['$large-indent']);
-        }
-        break;
-    }
-  };
-
-  parser.onclosetag = function (tagName) {
-    switch (tagName) {
-      case OsisTag.SECTION_HEADING:
-        verse.push([`$heading=${context.titleText.replace(/<(?:.|\n)*?>/gm, '')}`]);
-        context.currentNode = null;
-        context.title = null;
-        context.titleText = '';
-        break;
-      case OsisTag.NOTE:
-        context.noteText = '';
-        context.currentNote = null;
-        break;
-      case OsisTag.CROSS_REFERENCE:
-        context.currentRef = null;
-        break;
-      case OsisTag.QUOTE:
-        const isClosingQuotationMark = context.currentNode && context.currentNode.isSelfClosing
-          && context.currentNode.attributes.marker;
-        if (isClosingQuotationMark) {
-          // Add closing quote mark
-          verse.push([context.currentNode.attributes.marker]);
-        }
-        if (!context.currentNode) {
-          context.quote = null;
-        }
-        break;
-      case OsisTag.LINE_GROUP: // 'line group' (paragraph)
-        verse.push(['$paragraph-break']);
-        break;
-    }
-    context.lastTag = '';
-    context.currentNode = null;
-  };
 
   if (debugOutputEnabled) {
     // If developing, feel free to comment this out to pretty-print your XML.
@@ -176,61 +69,122 @@ function osis2sqlite(verseXML: string, debugOutputEnabled = false) {
   parser.write(verseXML);
   parser.close();
 
-  if (debugOutputEnabled) {
-    const renderedText = renderVerseAsFormattedText(verse);
-    console.log(renderedText);
-  }
-
   return verse;
 }
 
-/**
- * Render verse without any metadata or formatting information:
- * ['in the beginning, ', ['G1039']], ['God', ['G4932']] ==>
- * 'In the beginning, God'
- */
-function renderVerseAsPlainText(verse) {
-  let plaintext = '';
-  verse.forEach((verseBit) => {
-    if (!verseBit[0].includes('$')) {
-      plaintext += verseBit[0];
+function parseOpeningTag(node, verse, context: ParserContext) {
+  context.currentNode = node;
+  context.lastTag = node.name;
+  switch (node.name) {
+    case OsisTag.XML:
+      context.osisRef = node.attributes.osisRef;
+      context.verseNum = node.attributes.verseNum;
+      break;
+    case OsisTag.NOTE:
+      if (node.attributes.type !== 'crossReference') {
+        const osisRef = node.attributes.osisRef || node.attributes.annotateRef || context.osisRef;
+        if (!node.attributes.n) context.noteCount += 1;
+        const n = node.attributes.n || context.noteCount;
+        verse.push([`$note=${n}&osisRef=${osisRef}`]);
+      }
+      context.currentNote = node;
+      break;
+    case OsisTag.CROSS_REFERENCE:
+      context.currentRef = node;
+      break;
+    case OsisTag.SECTION_HEADING:
+      context.title = node;
+      break;
+    case OsisTag.POETIC_LINE:
+      if (node.attributes.level === Indentation.small && node.attributes.sID) {
+        verse.push(['$line-break']);
+        verse.push(['$small-indent']);
+      } else if (node.attributes.level === Indentation.large && node.attributes.sID) {
+        verse.push(['$line-break']);
+        verse.push(['$large-indent']);
+      }
+      break;
+  }
+};
+
+function parseClosingTag(tagName: string, verse, context: ParserContext) {
+  switch (tagName) {
+    case OsisTag.SECTION_HEADING:
+      verse.push([`$heading=${context.titleText.replace(/<(?:.|\n)*?>/gm, '')}`]);
+      context.currentNode = null;
+      context.title = null;
+      context.titleText = '';
+      break;
+    case OsisTag.NOTE:
+      context.noteText = '';
+      context.currentNote = null;
+      break;
+    case OsisTag.CROSS_REFERENCE:
+      context.currentRef = null;
+      break;
+    case OsisTag.QUOTE:
+      const isClosingQuotationMark = context.currentNode && context.currentNode.isSelfClosing
+        && context.currentNode.attributes.marker;
+      if (isClosingQuotationMark) {
+        // Add closing quote mark
+        verse.push([context.currentNode.attributes.marker]);
+      }
+      if (!context.currentNode) {
+        context.quote = null;
+      }
+      break;
+    case OsisTag.LINE_GROUP:
+      verse.push(['$paragraph-break']);
+      break;
+  }
+  context.lastTag = '';
+  context.currentNode = null;
+};
+
+function parseTextNode(text: string, verse, context: ParserContext) {
+  if (context.currentNote) {
+    processFootnotes(text, context);
+    return;
+  }
+  if (context.quote) {
+    const strongsNumbers = getStrongsNumbers(context);
+    if (context.quote.attributes.who === 'Jesus' && text) {
+      verse.push([`$redLetter=${text}`]);
+      return;
     }
-  });
-  // Remote duplicate whitespace
-  plaintext = plaintext.replace(/\s+/g, ' ');
-  plaintext += ' ';
-  return plaintext;
+    verse.push([text]);
+    if (strongsNumbers) {
+      verse[verse.length - 1].push(strongsNumbers);
+    }
+    return;
+  }
+  if (context.currentNode) {
+    switch (context.currentNode.name) {
+      case OsisTag.SECTION_HEADING:
+        context.titleText += text;
+        break;
+      case OsisTag.DIVINE_NAME:
+        if (context.title) {
+          const strongsNumbers = getStrongsNumbers(context);
+          verse.push([text, strongsNumbers]);
+        }
+        break;
+      default:
+        if (hasStrongsNumbers(context.currentNode)) {
+          const strongsNumbers = getStrongsNumbers(context);
+          verse.push([text, strongsNumbers]);
+          break;
+        }
+        verse.push([text]);
+        break;
+    }
+  } else {
+    verse.push([text]);
+  }
 }
 
-/**
- * Render verse with formatting information. This includes:
- * • Tabs
- * • Indentation
- * • Section headings
- */
-function renderVerseAsFormattedText(verse) {
-  let formattedText = '';
-  verse.forEach((verseBit) => {
-    if (verseBit[0].includes('$heading')) {
-      formattedText += '\n\n';
-      formattedText += verseBit[0].replace('$heading=', '');
-      formattedText += '\n\n';
-    } else if (verseBit[0].includes('$line-break')) {
-      formattedText += '\n';
-    } else if (verseBit[0].includes('$small-indent')) {
-      formattedText += '\t';
-    } else if (verseBit[0].includes('$large-indent')) {
-      formattedText += '\t\t';
-    } else if (verseBit[0].includes('$paragraph-break')) {
-      formattedText += '\n';
-    } else if (!verseBit[0].includes('$')) {
-      formattedText += verseBit[0];
-    }
-  });
-  // Remote duplicate whitespace
-  // formattedText = formattedText.replace(/\s+/g,' ');
-  formattedText += ' ';
-  return formattedText;
+function hasStrongsNumbers(currentNode) {
+  return 'attributes' in currentNode && 'lemma' in currentNode.attributes;
 }
 
 function getStrongsNumbers(context) {
